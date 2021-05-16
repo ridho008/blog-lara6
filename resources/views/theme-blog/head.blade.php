b6a997de853382d56c688e523c9adc8732d4630d
ridho008 <ciuyy12@gmail.com>
2021-05-16 13:29:51 +0700

    modified link navbar

--- resources/views/theme-blog/head.blade.php
+++ resources/views/theme-blog/head.blade.php
@@ -132,8 +132,7 @@
                      @endforeach
                   </ul>
                </li>
-               <li><a href="about.html">About Us</a></li>
-               <li><a href="contact.html">Contacts</a></li>
+               <li><a href="{{ route('blog.about') }}">About Us</a></li>
                {{-- <li><a href="#">Advertise</a></li> --}}
             </ul>
             <button class="nav-close nav-aside-close"><span></span></button>
